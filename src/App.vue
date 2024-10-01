f33823d77a1010bf39a2b1ef3479836c1f77f8e9
Arthur Miguel <jemergemer446@gmail.com>
2024-10-01 18:50:59 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,73 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Calculadora Aritmética</h1>
+    
+    <div class="calculator">
+      <input type="number" v-model="num1" placeholder="Número 1">
+      <select v-model="operation">
+        <option value="add">Somar</option>
+        <option value="subtract">Subtrair</option>
+        <option value="multiply">Multiplicar</option>
+        <option value="divide">Dividir</option>
+      </select>
+      <input type="number" v-model="num2" placeholder="Número 2">
+    </div>
+
+    <div class="result">
+      <h2>Resultado: {{ result }}</h2>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      num1: 0,
+      num2: 0,
+      operation: 'add',
+    };
+  },
+  computed: {
+    result() {
+      const n1 = parseFloat(this.num1);
+      const n2 = parseFloat(this.num2);
+
+      switch (this.operation) {
+        case 'add':
+          return n1 + n2;
+        case 'subtract':
+          return n1 - n2;
+        case 'multiply':
+          return n1 * n2;
+        case 'divide':
+          return n2 !== 0 ? n1 / n2 : 'Erro: Divisão por zero';
+        default:
+          return 0;
+      }
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 50px;
+}
+
+.calculator {
+  margin: 20px 0;
+}
+
+input, select {
+  margin: 10px;
+  padding: 5px;
+  font-size: 16px;
+}
+
+.result h2 {
+  margin-top: 20px;
+  color: #333;
 }
 </style>
